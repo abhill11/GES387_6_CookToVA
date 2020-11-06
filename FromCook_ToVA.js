2ad8d277c6867fd30db7a1198b279eb73fbf2486
abhill11 <70242250+abhill11@users.noreply.github.com>
2020-11-06 01:12:38 -0600

    upload files
    initial

---
+++ FromCook_ToVA.js
@@ -0,0 +1,12 @@
+var FromCook_ToVA = {
+"type": "FeatureCollection",
+"name": "5FromCook_ToVA",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "State_A": "Illinois", "County_A": "Cook County", "GEOID_A": "17031", "State_B": "Virginia", "County_B": "Henrico County", "GEOID_B": "51087", "Flow_B2A": 27, "Flow_A2B": 478, "Net_B2A": -451, "A_Lon": -87.8169, "A_Lat": 41.84, "B_Lon": -77.4062, "B_Lat": 37.5383 }, "geometry": { "type": "Point", "coordinates": [ -87.8169, 41.84 ] } },
+{ "type": "Feature", "properties": { "State_A": "Illinois", "County_A": "Cook County", "GEOID_A": "17031", "State_B": "Virginia", "County_B": "Fairfax County", "GEOID_B": "51059", "Flow_B2A": 334, "Flow_A2B": 639, "Net_B2A": -305, "A_Lon": -87.8169, "A_Lat": 41.84, "B_Lon": -77.2766, "B_Lat": 38.8358 }, "geometry": { "type": "Point", "coordinates": [ -87.8169, 41.84 ] } },
+{ "type": "Feature", "properties": { "State_A": "Illinois", "County_A": "Cook County", "GEOID_A": "17031", "State_B": "Virginia", "County_B": "Virginia Beach city", "GEOID_B": "51810", "Flow_B2A": 88, "Flow_A2B": 273, "Net_B2A": -185, "A_Lon": -87.8169, "A_Lat": 41.84, "B_Lon": -76.0434, "B_Lat": 36.7334 }, "geometry": { "type": "Point", "coordinates": [ -87.8169, 41.84 ] } },
+{ "type": "Feature", "properties": { "State_A": "Illinois", "County_A": "Cook County", "GEOID_A": "17031", "State_B": "Virginia", "County_B": "Norfolk city", "GEOID_B": "51710", "Flow_B2A": 39, "Flow_A2B": 109, "Net_B2A": -70, "A_Lon": -87.8169, "A_Lat": 41.84, "B_Lon": -76.2591, "B_Lat": 36.8945 }, "geometry": { "type": "Point", "coordinates": [ -87.8169, 41.84 ] } },
+{ "type": "Feature", "properties": { "State_A": "Illinois", "County_A": "Cook County", "GEOID_A": "17031", "State_B": "Virginia", "County_B": "Arlington County", "GEOID_B": "51013", "Flow_B2A": 199, "Flow_A2B": 244, "Net_B2A": -45, "A_Lon": -87.8169, "A_Lat": 41.84, "B_Lon": -77.1012, "B_Lat": 38.8786 }, "geometry": { "type": "Point", "coordinates": [ -87.8169, 41.84 ] } }
+]
+}
